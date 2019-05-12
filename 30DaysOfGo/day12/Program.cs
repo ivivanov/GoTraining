2ab5281f5d4884c4b05930555e3d822246772863
ivanttyy <ivanivanov12@gmail.com>
2019-05-12 22:45:31 +0300

    hacker rank 30 days of code does not support go code for this task

---
+++ 30DaysOfGo/day12/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+class Person
+{
+    protected string firstName;
+    protected string lastName;
+    protected int id;
+
+    public Person() { }
+    public Person(string firstName, string lastName, int identification)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = identification;
+    }
+    public void printPerson()
+    {
+        Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+    }
+}
+
+class Student : Person
+{
+    private int[] testScores;
+
+    /*	
+     *   Class Constructor
+     *   
+     *   Parameters: 
+     *   firstName - A string denoting the Person's first name.
+     *   lastName - A string denoting the Person's last name.
+     *   id - An integer denoting the Person's ID number.
+     *   scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
+    public Student(string firstName, string lastName, int identification, int[] scores) : base(firstName, lastName, identification)
+    {
+        testScores = scores;
+    }
+
+    /*	
+     *   Method Name: Calculate
+     *   Return: A character denoting the grade.
+     */
+    // Write your method here
+    public string Calculate()
+    {
+        int sum = testScores.Sum();
+        int avg = sum / testScores.Length;
+        if (90 <= avg && avg <= 100)
+            return "O";
+        if (80 <= avg && avg < 90)
+            return "E";
+        if (70 <= avg && avg < 80)
+            return "A";
+        if (55 <= avg && avg < 70)
+            return "P";
+        if (40 <= avg && avg < 55)
+            return "D";
+        if ( avg < 40)
+            return "T";
+
+        return string.Empty;
+    }
+}
+
+
+class Solution
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split();
+        string firstName = inputs[0];
+        string lastName = inputs[1];
+        int id = Convert.ToInt32(inputs[2]);
+        int numScores = Convert.ToInt32(Console.ReadLine());
+        inputs = Console.ReadLine().Split();
+        int[] scores = new int[numScores];
+        for (int i = 0; i < numScores; i++)
+        {
+            scores[i] = Convert.ToInt32(inputs[i]);
+        }
+
+        Student s = new Student(firstName, lastName, id, scores);
+        s.printPerson();
+        Console.WriteLine("Grade: " + s.Calculate() + "\n");
+    }
+}
